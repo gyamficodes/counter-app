ea100a05edb2b6d23fbe0a1164a61940e28a150d
gyamficodes <gyamfijohn08@gmail.com>
2023-06-18 22:29:52 -0700

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import { axios } from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    counter: 0,
   },
   getters: {
   },
   mutations: {
+    increaseCounter(state) {
+    state.counter++;
+    },
+    decreaseCounter(state) {
+      state.counter--;
+    },
   },
   actions: {
+    
   },
   modules: {
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,34 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <div class="counter">0</div>
-  <div class="cent">
-    <div class="buttons">
-      <button>-</button>
-      <button>+</button>
-    </div></div>
+    <div class="counter">{{ $store.state.counter }}</div>
+    <div class="cent">
+      <div class="buttons">
+        <button @click="$store.commit('decreaseCounter')">-</button>
+        <button @click="$store.commit('increaseCounter')">+</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+
 export default {
   name: "HomeView",
   components: {},
+  data(){
+    return{
+
+    }
+  },
+  methods: {
+  
+  },
+  computed: {},
+  mounted(){},
+  created(){},
 };
 </script>
 <style lang="scss">
